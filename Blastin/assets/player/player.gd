d3b93a28c835d64fd7bace3c4a20808d85388472
pattlebass <49322676+pattlebass@users.noreply.github.com>
2020-01-05 23:51:17 +0200

    Add files

---
+++ Blastin/assets/player/player.gd
@@ -0,0 +1,41 @@
+extends RigidBody2D
+
+var speed = 400
+var jump_force = 600
+var on_floor = false
+var jump_counter = 0
+
+func _ready():
+	#Add self as exception to floor detection rays
+	$"floor detection/RayCast2D".add_exception(self)
+	$"floor detection/RayCast2D2".add_exception(self)
+	$"floor detection/RayCast2D3".add_exception(self)
+
+#func _process(delta):
+#	pass
+
+func _integrate_forces(state):
+	
+	#Left and right
+	
+	if Input.is_action_pressed("left"):
+		linear_velocity.x = -speed
+	if Input.is_action_pressed("right"):
+		linear_velocity.x = speed
+	
+	#Jumping
+	
+	if Input.is_action_just_pressed("up") and (on_floor or jump_counter < 1):
+		apply_central_impulse(Vector2(0, -jump_force))
+		jump_counter += 1
+
+	#Floor detection
+	
+	for ray in $"floor detection".get_children():
+		if ray.is_colliding():
+			on_floor = true
+			jump_counter = 0
+		else:
+			on_floor = false
+	
+
